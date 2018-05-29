c71b40b5886ee2b4418a88a176420bfa0a0ccbec
Beastie PC <gskondratenko@opora.org.ua>
2018-05-30 01:22:12 +0300

    Changa parserf or new site rada

--- lib/scrape_all_page.rb
+++ lib/scrape_all_page.rb
@@ -1,24 +1,20 @@
 require_relative 'get_page'
-require_relative 'scrape_all_votes'
+#require_relative 'scrape_all_votes'
 require_relative 'get_mps'
-require_relative 'voted'
+#require_relative 'voted'
 
 class GetPages
   def initialize
     @all_page = []
     $all_mp = GetMp.new
-    @vot_seved =  VoteEvent.all(:fields => [:date_caden], :order => [:date_caden]).map{|v| v.date_caden}.uniq
+    #@vot_seved =  VoteEvent.all(:fields => [:date_caden], :order => [:date_caden]).map{|v| v.date_caden}.uniq
     url = "https://www.lutskrada.gov.ua/pages/rezuaty-holosuvan-zasidan-sesii"
     page = GetPage.page(url)
-    check = nil
-    page.css('.even p').each do |p|
-      if p.text == "Міська рада 7-го скликання"
-        check = 1
-      end
-      next if check.nil?
-      next if p.text == "Міська рада 7-го скликання"
-      next if p.text == ""
-      @all_page << { cadent: p.text[/\(.+\)/].gsub(/(\(|\))/,''), url: p.css('a')[0][:href] }
+    page.css('.c-text p').each do |p|
+      next if p.css('a').empty?
+      p p.css('a')[0][:href].gsub('https://www.lutskrada.gov.ua', '')
+      p p.text[/\(.+\)/].gsub(/(\(|\))/,'')
+      @all_page << { cadent: p.text[/\(.+\)/].gsub(/(\(|\))/,''), url: p.css('a')[0][:href].gsub('https://www.lutskrada.gov.ua', '') }
     end
   end
   def get_all_votes
@@ -29,3 +25,4 @@ class GetPages
     end
   end
 end
+GetPages.new
